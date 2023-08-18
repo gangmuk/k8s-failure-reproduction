ba8118bf564a663756d92ae120b7187841841f68
gangmuk <gangmuk@gmail.com>
2023-08-18 15:40:26 -0500

    Update H1/kubectl_command.sh

--- failure_cases/H1/kubectl_command.sh
+++ failure_cases/H1/kubectl_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/gangmuk2/project/k8s-failure-reproduction/logging/bin/time_function.sh
+source /home/gangmuk2/projects/k8s-failure-reproduction/logging/bin/time_function.sh
 
 update_time
 fn=${CDT}-command-H1.log.csv
@@ -8,12 +8,21 @@ fn=${CDT}-command-H1.log.csv
 echo "command,keyword,start_cdt,start_utc,end_cdt,end_utc" > ${fn}
 
 update_time
-echo -n "kubectl apply -f deploy_h1-app.yaml,apply custom h1 app deployment,${CDT},${UTC}," >> ${fn}
+echo -n "python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_start.py,start logging,${CDT},${UTC}," >> ${fn}
+echo "logging start"
+python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_start.py &
+
+update_time
+echo "${CDT},${UTC}" >> ${fn}
+sleep 10
+
+update_time
+echo -n "kubectl apply -f deploy_h1-app.yaml,apply deployment h1-app,${CDT},${UTC}," >> ${fn}
 kubectl apply -f deploy_h1-app.yaml
 
 update_time
 echo "${CDT},${UTC}" >> ${fn}
-sleep 1
+sleep 5
 
 update_time
 #echo -n "kubectl autoscale deployment h1-app --cpu-percent=50 --min=1 --max=10,deploy autoscaler,${CDT},${UTC}," >> ${fn}
@@ -24,17 +33,7 @@ kubectl apply -f autoscaler.yaml
 
 update_time
 echo "${CDT},${UTC}" >> ${fn}
-sleep 1
-
-update_time
-echo -n "python3 /home/gangmuk2/project/k8s-failure-reproduction/logging/logging_start.py,start logging,${CDT},${UTC}," >> ${fn}
-echo "logging start"
-python3 /home/gangmuk2/project/k8s-failure-reproduction/logging/logging_start.py &
-
-update_time
-echo "${CDT},${UTC}" >> ${fn}
-sleep 600
-
+sleep 500
 
 update_time
 echo -n "pkill -f \"python3 logging_start.py\",stop logging_start.py,${CDT},${UTC}," >> ${fn}
@@ -46,8 +45,8 @@ echo "${CDT},${UTC}" >> ${fn}
 sleep 5
 
 update_time
-echo -n "python3 /home/gangmuk2/project/k8s-failure-reproduction/logging/logging_end.py,end logging,${CDT},${UTC}," >> ${fn}
-python3 /home/gangmuk2/project/k8s-failure-reproduction/logging/logging_end.py
+echo -n "python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_end.py,end logging,${CDT},${UTC}," >> ${fn}
+python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_end.py
 
 update_time
 echo "${CDT},${UTC}" >> ${fn}
