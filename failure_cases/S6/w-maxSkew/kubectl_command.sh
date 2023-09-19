c52990b02bdb9ea2ce381313e71bc19035830152
gangmuk <gangmuk@gmail.com>
2023-09-19 03:16:34 -0500

    Update S6 with maxSkew log

--- failure_cases/S6/w-maxSkew/kubectl_command.sh
+++ failure_cases/S6/w-maxSkew/kubectl_command.sh
@@ -1,59 +1,83 @@
 #!/bin/bash
 
-source /home/gangmuk2/project/k8s-failure-reproduction/logging/bin/time_function.sh
+source /home/gangmuk2/projects/k8s-failure-reproduction/logging/bin/time_function.sh
 
-cdt_time
+update_time
 fn=kubectl_command-S6-${UTC}.log.txt
 
 echo "command,start_cdt,start_utc,end_cdt,end_utc" >> ${fn}
 
-cdt_time
-utc_time
-echo -n "kubectl apply -f deploy.yaml,${CDT},${UTC}," >> ${fn}
+update_time
+echo -n "python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_start.py,start logging
+,${CDT},${UTC}," >> ${fn}
+echo "logging start"
+python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_start.py &
+
+update_time
+echo "${CDT},${UTC}" >> ${fn}
+sleep 10
+
+update_time
+echo -n "kubectl apply -f deploy.yaml,apply deployment php-apache,${CDT},${UTC}," >> ${fn}
 kubectl apply -f deploy.yaml
-cdt_time
-utc_time
-echo "${CDT},${UTC}" >> ${fn}
 
+update_time
+echo "${CDT},${UTC}" >> ${fn}
 sleep 10
 
-cdt_time
-utc_time
-echo -n "kubectl drain kind-worker --ignore-daemonsets --delete-emptydir-data,${CDT},${UTC}," >> ${fn}
+update_time
+echo -n "kubectl drain kind-worker --ignore-daemonsets --delete-emptydir-data,drain kind-worker node,${CDT},${UTC}," >> ${fn}
 kubectl drain kind-worker --ignore-daemonsets --delete-emptydir-data
-cdt_time
-utc_time
+
+update_time
 echo "${CDT},${UTC}" >> ${fn}
+sleep 15
 
-sleep 5
-
-cdt_time
-utc_time
-echo -n "kubectl uncordon kind-worker,${CDT},${UTC}," >> ${fn}
+update_time
+echo -n "kubectl uncordon kind-worker,uncordon kind-worker,${CDT},${UTC}," >> ${fn}
 kubectl uncordon kind-worker
-cdt_time
-utc_time
+
+update_time
 echo "${CDT},${UTC}" >> ${fn}
+sleep 15
 
-sleep 5
-
-cdt_time
-utc_time
-echo -n "kubectl drain kind-worker3 --ignore-daemonsets --delete-emptydir-data,${CDT},${UTC}," >> ${fn}
+update_time
+echo -n "kubectl drain kind-worker3 --ignore-daemonsets --delete-emptydir-data,drain kind-worker3,${CDT},${UTC}," >> ${fn}
 kubectl drain kind-worker3 --ignore-daemonsets --delete-emptydir-data
-cdt_time
-utc_time
-echo "${CDT},${UTC}" >> ${fn}
 
+update_time
+echo "${CDT},${UTC}" >> ${fn}
+sleep 15
+
+update_time
+echo -n "kubectl uncordon kind-worker3,uncordon kind-worker3,${CDT},${UTC}," >> ${fn}
+kubectl uncordon kind-worker3
+
+update_time
+echo "${CDT},${UTC}" >> ${fn}
+sleep 15
+
+update_time
+echo -n "pkill -f \"python3 logging_start.py\",stop logging_start.py,${CDT},${UTC}," >> ${fn}
+pkill -f "logging_start.py"
+echo "pkill -f \"logging_start.py\""
+
+update_time
+echo "${CDT},${UTC}" >> ${fn}
 sleep 5
 
-cdt_time
-utc_time
-echo -n "kubectl uncordon kind-worker3,${CDT},${UTC}," >> ${fn}
-kubectl uncordon kind-worker3
-cdt_time
-utc_time
-echo "${CDT},${UTC}" >> ${fn}
+update_time
+echo -n "python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_end.py,end logging,${CDT},${UTC}," >> ${fn}
+python3 /home/gangmuk2/projects/k8s-failure-reproduction/logging/logging_end.py
 
-sleep 10
+update_time
+echo "${CDT},${UTC}" >> ${fn}
+sleep 5
+
+update_time
+echo -n "kubectl delete deploy/php-apache,delete php-apache deployment,${CDT},${UTC}," >> ${fn}
 kubectl delete deploy/php-apache
+
+update_time
+echo "${CDT},${UTC}" >> ${fn}
+sleep 10
